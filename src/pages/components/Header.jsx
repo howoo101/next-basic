5d8dca7d1e9497dfd8f2cf13c86bfbeab3912443
homean <hohohosky101@gmail.com>
2023-08-03 13:15:30 +0900

    헤더 컴포넌트 추가

---
+++ src/pages/components/Header.jsx
@@ -0,0 +1,38 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import styles from './Header.module.scss';
+
+function Header() {
+	const router = useRouter();
+	const currentPath = router.pathname;
+
+	return (
+		<header id={styles.header}>
+			<h1>
+				<Link href='/' className={currentPath === '/' ? styles.on : ''}>
+					Logo
+				</Link>
+			</h1>
+
+			<ul id={styles.gnb}>
+				<li>
+					<Link href='/ssg' className={currentPath === '/ssg' ? styles.on : ''}>
+						SSG
+					</Link>
+				</li>
+				<li>
+					<Link href='/ssr' className={currentPath === '/ssr' ? styles.on : ''}>
+						SSR
+					</Link>
+				</li>
+				<li>
+					<Link href='/isr' className={currentPath === '/isr' ? styles.on : ''}>
+						ISR
+					</Link>
+				</li>
+			</ul>
+		</header>
+	);
+}
+
+export default Header;

2845fd0d91bd8b1eaab5afa70d36451b873c9f43
homean <hohohosky101@gmail.com>
2023-08-03 13:16:03 +0900

    컴포넌트에 헤더 추가

--- src/pages/components/Sublayout.jsx
+++ src/pages/components/Sublayout.jsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Header from './Header';
 
 function Sublayout(props) {
 	return (
@@ -7,6 +8,7 @@ function Sublayout(props) {
 				<title>{props.name}</title>
 			</Head>
 			<section>
+				<Header />
 				<h1>{props.name}</h1>
 				{props.children}
 			</section>

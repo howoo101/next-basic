f7d6220c40145164f8275f5f7327218d2c36c1f7
homean <hohohosky101@gmail.com>
2023-08-03 12:56:49 +0900

    404 페이지

---
+++ src/pages/404.jsx
@@ -0,0 +1,11 @@
+import Sublayout from './components/Sublayout';
+
+function NotFound() {
+	return (
+		<Sublayout name={'404'}>
+			<h1>404페이지 입니다.</h1>
+		</Sublayout>
+	);
+}
+
+export default NotFound;

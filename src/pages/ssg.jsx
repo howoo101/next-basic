f42131ab270363d9ee6cb705149782b62aa34654
homean <hohohosky101@gmail.com>
2023-08-04 13:18:32 +0900

    ssg 방식에 멤버 데이터 포함

--- src/pages/ssg.jsx
+++ src/pages/ssg.jsx
@@ -1,11 +1,19 @@
 import Sublayout from './components/Sublayout';
-
+import data from '@/public/members.json';
 function ssg(props) {
-	console.log(props);
 	return (
 		<Sublayout name={'SSG'}>
 			<p>ssg 방식 테스트 페이지</p>
 			<h1>{props.now}</h1>
+
+			{props.members.map((member, idx) => {
+				return (
+					<article key={idx}>
+						<h2>{member.name}</h2>
+						<p>{member.position}</p>
+					</article>
+				);
+			})}
 		</Sublayout>
 	);
 }
@@ -14,7 +22,7 @@ export async function getStaticProps() {
 	console.log('ssg');
 	//해당 프로젝트를 빌드시에 한번만 호출
 	return {
-		props: { now: performance.now() },
+		props: { now: performance.now(), members: data.members },
 	};
 }
 

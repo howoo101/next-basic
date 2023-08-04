29d93c76798cddf37c012bd981ed038ad0bdc794
homean <hohohosky101@gmail.com>
2023-08-04 10:36:04 +0900

    한글 폰트 적용

--- src/pages/components/Sublayout.jsx
+++ src/pages/components/Sublayout.jsx
@@ -1,10 +1,10 @@
 import Head from 'next/head';
 import Header from './Header';
 import styles from './SubLayout.module.scss';
-import { Orbitron } from 'next/font/google';
+import { Orbitron, Noto_Sans_KR } from 'next/font/google';
 
 const orbitron = Orbitron({ subsets: ['latin'], weight: ['400', '500'], preload: true });
-console.log(orbitron);
+const notoSans = Noto_Sans_KR({ subsets: ['latin'], weight: ['100', '300'], preload: true });
 
 function Sublayout(props) {
 	return (
@@ -15,7 +15,8 @@ function Sublayout(props) {
 			<section>
 				<Header />
 				<div className={styles.subLayout}>
-					<h1>{props.name}</h1>
+					<h1 className={orbitron.className}>{props.name}</h1>
+					<p className={notoSans.className}>레이아웃 컨텐츠 페이지입니다.</p>
 					{props.children}
 				</div>
 			</section>

52fbb47f56391a7e94a7dcc6ea3e69f15eb91ddc
homean <hohohosky101@gmail.com>
2023-08-09 09:55:51 +0900

    구글 웹 폰트 적용

--- src/pages/components/Header.jsx
+++ src/pages/components/Header.jsx
@@ -1,13 +1,23 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import styles from './Header.module.scss';
+import { Orbitron } from 'next/font/google';
+// 구글 웹폰트 시스템이 내장되어 있음
+//next자체적으로 구글웹폰트의 용량을 최적화 시켜서 페이지에 적용
+//비구조화할당으로 원하는 폰트 객체를 import
+
+//Orbitron생성자로부터 사용할 객체를 반환 (subsets: 언어타입, weight: [사용할 폰트 굵기] , preload: 사전로딩유무)
+//weight값이 등록되지않으면 콘솔 오류
+//반환된 폰트객체 안에는 클래스명이 프로퍼티로 등록되어 있음
+//해당 폰트를 사용하고자 하는 부모요소에 해당객체의 클래스명을 등록하면 자식컴포넌트에서는 자유롭게 해당 폰트 사용가능
+const orbitron = Orbitron({ subsets: ['latin'], weight: ['400', '500'], preload: true });
 
 function Header() {
 	const router = useRouter();
 	const currentPath = router.pathname;
 
 	return (
-		<header id={styles.header}>
+		<header id={styles.header} className={orbitron.className}>
 			<h1>
 				<Link href='/' className={currentPath === '/' ? styles.on : ''}>
 					Logo

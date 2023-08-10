f50a32428e5a77ee0a9215d6b2218c4d80b13fed
homean <hohohosky101@gmail.com>
2023-08-10 09:54:41 +0900

    api router

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,29 @@ import styles from '@/src/styles/Home.module.scss';
 import Header from './components/Header';
 import pic from '@/public/img/pic.jpg';
 import Image from 'next/image';
+import { useEffect, useRef, useState } from 'react';
+
+//api 라우팅 (서버요청 처리를 위해서는 express라는 프레임웍을 활용)
+//next에서는 api폴더 안쪽에 서버쪽 요청 및 응답에대한 라우팅 설정가능
+//api폴더 안쪽의 파일명이 라우터 요청명으로 자동설정됨 /api/hello
 
 export default function Home() {
+	//서버쪽에서 프리렌더된 페이지를 가지고온 이후에
+	//클라이언트쪽에서 다시 서버쪽 요청가능
+	//next자체적으로 서버쪽 요청, 응답처리
+
+	fetch('/api/hello')
+		.then((response) => response.json())
+		.then((json) => console.log(json));
+
+	useEffect(() => {
+		fetch('/api/hello', {
+			method: 'POST',
+			body: 'ABC',
+		})
+			.then((res) => res.json())
+			.then((json) => console.log(json));
+	});
 	return (
 		<>
 			<Head>
@@ -16,6 +37,7 @@ export default function Home() {
 			<main className={styles.main}>
 				<Header />
 				<h1>Main</h1>
+
 				<div className={styles.pic}>
 					<Image src={pic} alt='pic' placeholder='blur' fill quality={50} />
 				</div>

a7d99d487850003aa40f2aa123760cd90048254e
homean <hohohosky101@gmail.com>
2023-08-10 11:07:14 +0900

    Mongo connection test

--- src/pages/api/post.jsx
+++ src/pages/api/post.jsx
@@ -1,9 +1,14 @@
+import { connectMongoDB } from '@/src/libs/MongoConnect';
+
 //안쪽에서 await로 동기화 처리할 예정이므로 wrapping함수에 async지정
 export default async function handler(req, res) {
 	//await로 동기화하므로 then,catch문을 쓸수 없으니 try, catch문으로 예외처리
 	try {
-		//요청 성공시 실행될 구문
+		const DB = await connectMongoDB();
+
+		res.status(200).send('success');
 	} catch (err) {
 		//요청 실패시 실행될 구문
+		res.send('error');
 	}
 }

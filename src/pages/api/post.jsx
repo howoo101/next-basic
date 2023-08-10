150905dc487ef0ce31a9159a4822af690a17d990
homean <hohohosky101@gmail.com>
2023-08-10 12:57:48 +0900

    create

--- src/pages/api/post.jsx
+++ src/pages/api/post.jsx
@@ -1,14 +1,37 @@
 import { connectMongoDB } from '@/src/libs/MongoConnect';
+import { Community } from '@/src/model/CommunitySchema';
+import { Counter } from '@/src/model/CounterSchema';
 
 //안쪽에서 await로 동기화 처리할 예정이므로 wrapping함수에 async지정
 export default async function handler(req, res) {
-	//await로 동기화하므로 then,catch문을 쓸수 없으니 try, catch문으로 예외처리
-	try {
-		const DB = await connectMongoDB();
+	const temp = req.body;
 
-		res.status(200).send('success');
-	} catch (err) {
-		//요청 실패시 실행될 구문
-		res.send('error');
+	if (req.method === 'POST') {
+		try {
+			await connectMongoDB();
+			Counter.findOne({ name: 'counter' })
+				.exec()
+				.then((doc) => {
+					// 카운터 모델에서 글번호 가져와서 글번호 추가
+					temp.communityNum = doc.communityNum;
+
+					const communityModel = new Community(temp);
+					communityModel
+						.save()
+						.then(() => {
+							// 글저장 완료되면 글번호 1증가
+							Counter.updateOne(({ name: 'counter' }, { $inc: { communityNum: 1 } }))
+								.exec()
+								.then(() => {
+									res.json({ success: true });
+								});
+						})
+						.catch((err) => res.json({ success: false, err: err }));
+				});
+		} catch (err) {
+			//요청 실패시 실행될 구문
+			console.log(err);
+			res.send('error!');
+		}
 	}
 }
--- src/pages/post/index.jsx
+++ src/pages/post/index.jsx
@@ -1,11 +1,56 @@
+import { useEffect, useState } from 'react';
 import Sublayout from '../components/Sublayout';
+import axios from 'axios';
 
-function post() {
+function Post() {
+	const [title, setTitle] = useState('');
+	const [content, setContent] = useState('');
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const item = {
+			title: title,
+			content: content,
+		};
+		console.log(item);
+		axios
+			.post('/api/post', item)
+			.then((res) => {
+				console.log(res);
+				alert('글 저장 성공');
+			})
+			.catch((err) => {
+				console.log(err);
+				alert('글 저장 실패');
+			});
+	};
+
+	useEffect(() => {});
+
 	return (
 		<Sublayout name={'POST'}>
 			<p>포스트페이지 인트로 화면입니다.</p>
+
+			<div className='inputBox'>
+				<form onSubmit={handleSubmit}>
+					<input type='text' placeholder='입력해라 제목' value={title} onChange={(e) => setTitle(e.target.value)} />
+					<textarea
+						name=''
+						id=''
+						cols='30'
+						rows='10'
+						placeholder='입력해라 본문'
+						onChange={(e) => setContent(e.target.value)}
+					></textarea>
+
+					<nav>
+						<input type='reset' defaultValue='리셋' />
+						<input type='submit' defaultValue='제출' />
+					</nav>
+				</form>
+			</div>
 		</Sublayout>
 	);
 }
 
-export default post;
+export default Post;

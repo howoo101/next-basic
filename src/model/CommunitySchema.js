ea1e808eed7ecd9f172f9da67ad7269e243faa52
homean <hohohosky101@gmail.com>
2023-08-10 11:18:01 +0900

    스키마 생성

---
+++ src/model/CommunitySchema.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const communitySchema = new mongoose.Schema(
+	{
+		name: String,
+		content: String,
+		communityNum: Number,
+	},
+	{ collection: 'Community' }
+);
+
+const Community = mongoose.model('Community', communitySchema);
+export { Community };
---
+++ src/model/CounterSchema.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const counterSchema = new mongoose.Schema(
+	{
+		name: String,
+		communityNum: Number,
+	},
+	{ collection: 'Counter' }
+);
+
+const Counter = mongoose.model('Counter', counterSchema);
+export { Counter };

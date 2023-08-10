da88b4ceac612d4c5d9f0af1e2121f00470eef75
homean <hohohosky101@gmail.com>
2023-08-10 10:06:53 +0900

    DB데이터 입출력을 위한 전용 라우터 생성

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,22 +10,6 @@ import { useEffect, useRef, useState } from 'react';
 //api폴더 안쪽의 파일명이 라우터 요청명으로 자동설정됨 /api/hello
 
 export default function Home() {
-	//서버쪽에서 프리렌더된 페이지를 가지고온 이후에
-	//클라이언트쪽에서 다시 서버쪽 요청가능
-	//next자체적으로 서버쪽 요청, 응답처리
-
-	fetch('/api/hello')
-		.then((response) => response.json())
-		.then((json) => console.log(json));
-
-	useEffect(() => {
-		fetch('/api/hello', {
-			method: 'POST',
-			body: 'ABC',
-		})
-			.then((res) => res.json())
-			.then((json) => console.log(json));
-	});
 	return (
 		<>
 			<Head>
